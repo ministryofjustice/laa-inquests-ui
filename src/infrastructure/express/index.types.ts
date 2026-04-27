396f714da0cc19c2eb730055c800808cea3ade25
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-04-27 16:16:22 +0100

    [IDDS-24] Remove axios middleware

--- src/infrastructure/express/index.types.ts
+++ src/infrastructure/express/index.types.ts
@@ -1,10 +1,8 @@
-import type { AxiosInstanceWrapper } from "#src/infrastructure/express/middleware/axios/index.types.js";
 import type { ExpressLocaleLoader } from "#src/infrastructure/express/middleware/nunjucks/i18nLoader.js";
 
 declare global {
   namespace Express {
     interface Request {
-      axiosMiddleware: AxiosInstanceWrapper;
       locale: ExpressLocaleLoader;
     }
   }
